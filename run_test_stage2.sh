65e472d2a34f21090b5a67e65565563a6a16c1c4
Qichang Zhang <z18954061805@163.com>
2025-04-03 09:45:22 +0800

    stage2 inference

--- run_test_stage2.sh
+++ run_test_stage2.sh
@@ -4,8 +4,7 @@ python3  stage2_batchtest_inpaint_model.py \
  --pretrained_model_name_or_path="stabilityai/stable-diffusion-2-1-base" \
  --image_encoder_g_path="laion/CLIP-ViT-H-14-laion2B-s32B-b79K" \
  --image_encoder_p_path='facebook/dinov2-giant' \
- --img_path='{image_path}' \
- --pose_path="{pose_path}" \
+ --img_path='/root/autodl-tmp/data/train' \
  --target_embed_path="./logs/view_stage1/512_512/" \
  --save_path="./logs/view_stage2/384_512" \
  --step_number=12000 \
